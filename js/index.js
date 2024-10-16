2fbbe4de3abafeab0476347f3bbae3a76792f144
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-10-16 10:07:24 +0300

    fox1

---
+++ js/index.js
@@ -0,0 +1,9 @@
+document
+  .getElementById("add-msg-for")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let userName = document.getElementById("name-inp").value;
+    let msg = document.getElementById("msg-inp").value;
+    const itemMsg = document.createElement("li");
+  });
