388a0c258dfdbe32f9c336492d5a214b11ad53be
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-10-16 14:33:51 +0300

    fox2

--- js/index.js
+++ js/index.js
@@ -1,3 +1,31 @@
+let data = localStorage.getItem("msgList");
+let msgList = [];
+
+if (data !== "" && data !== null) {
+  msgList = JSON.parse(data);
+}
+
+function createNewMsg(obj) {
+  const itemMsg = document.createElement("li");
+  itemMsg.classList.add("list-group-item");
+
+  const itemTitle = document.createElement("h2");
+  itemTitle.textContent = obj.name;
+
+  const itemText = document.createElement("p");
+  itemText.classList.add("lead");
+  itemText.textContent = obj.msg;
+
+  itemMsg.append(itemTitle);
+  itemMsg.append(itemText);
+
+  document.getElementById("msg-list").append(itemMsg);
+}
+
+for (const msg of msgList) {
+  createNewMsg(msg);
+}
+
 document
   .getElementById("add-msg-for")
   .addEventListener("submit", function (event) {
@@ -5,5 +33,26 @@ document
 
     let userName = document.getElementById("name-inp").value;
     let msg = document.getElementById("msg-inp").value;
-    const itemMsg = document.createElement("li");
+
+    let msgObj = {
+      name: userName,
+      msg: msg,
+    };
+
+    msgList.push(msgObj);
+
+    msgList.push(msgObj);
+
+    localStorage.setItem("msgList", JSON.stringify(msgList));
+
+    createNewMsg(msgObj);
+
+    document.getElementById("msg-inp").value = "";
   });
+
+// localStorage.setItem("name", "Илья");
+// localStorage.setItem("age", 12);
+// localStorage.setItem("data", JSON.stringify({ name: "e213" }));
+// console.log(localStorage.getItem("name"));
+// console.log(localStorage.getItem("age"));
+// console.log(JSON.parse(localStorage.getItem("data")));
